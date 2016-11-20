378e20b8d7e15dadfae10c522636f5dfc95dfa90
learner <roadoflearning@live.com>
2016-11-20 18:28:00 +0800

    增加 Result 类

---
+++ src/main/java/com/zheteng123/jersey/pojo/Result.java
@@ -0,0 +1,35 @@
+package com.zheteng123.jersey.pojo;
+
+/**
+ * Created on 2016/11/20.
+ */
+public class Result<T> {
+
+    private int code;
+    private String message;
+    private T data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
