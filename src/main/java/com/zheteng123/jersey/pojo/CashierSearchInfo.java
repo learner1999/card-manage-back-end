3e932e63eaef1389a41d4e4959c5b29827a23af7
learner <roadoflearning@live.com>
2016-12-10 16:36:10 +0800

    新增 商家收银台相关接口

---
+++ src/main/java/com/zheteng123/jersey/pojo/CashierSearchInfo.java
@@ -0,0 +1,26 @@
+package com.zheteng123.jersey.pojo;
+
+/**
+ * Created on 2016/12/9.
+ */
+public class CashierSearchInfo {
+
+    private String id;
+    private int storeId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+}
